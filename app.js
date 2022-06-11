1e8269c873a494d4cdb66150239e74a0683c3da3
dhruvchodha7 <dhruvchodha@live.com>
2022-06-11 16:15:23 +0530

    made views and edited app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const {PORT} = process.env;
+const hbs = require('hbs')
+const { urlencoded } = require('express');
 
+const userRoute = require('./route/user');
+const homeRoute = require('./route/home');
 
-
-
-
+//middleware
+app.use(urlencoded({extended: false}))
+app.use(morgan('tiny'));
+//template engine
+app.set('view engine', 'hbs');
 
 
 module.exports = app;
